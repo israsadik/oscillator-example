b999f82892edf940412f337bf461d9e816d4e1bf
Isra Sadik <isra_s_@hotmail.com>
2026-05-27 13:05:23 +0200

    Update trajectory computation

--- numerical_studies/compute_trajectories.py
+++ numerical_studies/compute_trajectories.py
@@ -6,25 +6,28 @@ import run_partitioned_simulation as rps
 
 
 def compute_newmark_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
-    num_sol = rps.partitioned_newmark_beta(t_stop, N, coupling_scheme, **kwargs)
-    return num_sol
+    return rps.partitioned_newmark_beta(t_stop, N, coupling_scheme, **kwargs)
 
 
 def compute_alpha_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
-    num_sol = rps.partitioned_generalized_alpha(t_stop, N, coupling_scheme, **kwargs)
-    return num_sol
+    return rps.partitioned_generalized_alpha(t_stop, N, coupling_scheme, **kwargs)
+
+
+def compute_erk2_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
+    return rps.partitioned_erk(t_stop, N, 2, coupling_scheme, **kwargs)
 
 
 def compute_erk4_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
-    num_sol = rps.partitioned_erk(t_stop, N, 4, coupling_scheme, **kwargs)
-    return num_sol
+    return rps.partitioned_erk(t_stop, N, 4, coupling_scheme, **kwargs)
 
 
 def compute_sie_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
-    num_sol = rps.partitioned_semi_implicit_euler(t_stop, N, coupling_scheme, **kwargs)
-    return num_sol
+    return rps.partitioned_semi_implicit_euler(t_stop, N, coupling_scheme, **kwargs)
 
 
 def compute_mid_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
-    num_sol = rps.partitioned_implicit_midpoint(t_stop, N, coupling_scheme, **kwargs)
-    return num_sol
+    return rps.partitioned_implicit_midpoint(t_stop, N, coupling_scheme, **kwargs)
+
+
+def compute_trap_trajectory(t_stop: float, N: int, coupling_scheme: str, **kwargs):
+    return rps.partitioned_trapezoidal(t_stop, N, coupling_scheme, **kwargs)
